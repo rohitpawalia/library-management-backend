8172bbc7ff5d5070bf8bcf920b85b09be2cc62af
Rohit Yadav <rohit@Rohit-Yadav.local>
2026-09-15 18:05:09 +0530

    Temporarily disable AI route

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bookRoutes from "./routes/bookRoutes";
 import errorMiddleware from "./middleware/errorMiddleware";
 import cors from 'cors';
-import aiRoutes from "./routes/aiRoutes";
+// import aiRoutes from "./routes/aiRoutes";
 
 const app = express();
 app.use(cors());
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(bookRoutes);
-app.use(aiRoutes);
+// app.use(aiRoutes);
 app.use(errorMiddleware);
 
 export default app;
